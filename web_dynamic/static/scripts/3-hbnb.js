642d0d3a88ed7656077b92c843488bf0b8d0b5e9
thisisteey <dadataiwo82@yahoo.com>
2024-04-05 06:00:42 -0700

    JavaScript that requests and displays places dynamically from backend endpoint to frontend using jQuery

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,137 @@
+// Script that requests and displays places dynamically from backend endpoint to frontend
+'use strict';
+$(() => {
+  let selectedAmenities = [];
+  const selectors = {
+    amenitiesHeader: '.amenities > h4',
+    amenityCheckBox: '.amenities > .popover > ul > li > input[type="checkbox"]',
+    amenityListItem: '.amenities > .popover > ul > li'
+  };
+  const API_URL = 'http://localhost:5001/api/v1';
+
+  const buildPlaceElement = place => {
+    if (place) {
+      const article = document.createElement('article');
+
+      const titleBox = document.createElement('div');
+      titleBox.className = 'title_box';
+      const titleHTML = `<h2>${place.name}</h2>`;
+      const priceByNightHTML = '<div class="price_by_night">' +
+        `$${place.price_by_night}` +
+        '</div>';
+      titleBox.insertAdjacentHTML('beforeend', titleHTML);
+      titleBox.insertAdjacentHTML('beforeend', priceByNightHTML);
+
+      const informationBox = document.createElement('div');
+      informationBox.className = 'information';
+      const maxGuestHTML = '<div class="max_guest">' +
+        `${place.max_guest}` +
+        ` Guest${place.max_guest !== 1 ? 's' : ''}` +
+        '</div>';
+      const numberRoomsHTML = '<div class="number_rooms">' +
+        `${place.number_rooms}` +
+        ` Bedroom${place.number_rooms !== 1 ? 's' : ''}` +
+        '</div>';
+      const numberBathroomsHTML = '<div class="number_bathrooms">' +
+        `${place.number_bathrooms}` +
+        ` Bathroom${place.number_bathrooms !== 1 ? 's' : ''}` +
+        '</div>';
+      informationBox.insertAdjacentHTML('beforeend', maxGuestHTML);
+      informationBox.insertAdjacentHTML('beforeend', numberRoomsHTML);
+      informationBox.insertAdjacentHTML('beforeend', numberBathroomsHTML);
+
+      const userBox = document.createElement('div');
+      userBox.className = 'user';
+      if (place.user) {
+        const userHTML = '<b>Owner:</b>' +
+          ` ${place.user.first_name} ${place.user.last_name}`;
+        userBox.insertAdjacentHTML('beforeend', userHTML);
+      }
+
+      const descriptionBox = document.createElement('div');
+      descriptionBox.className = 'description';
+      descriptionBox.innerHTML = place.description;
+
+      article.insertAdjacentElement('beforeend', titleBox);
+      article.insertAdjacentElement('beforeend', informationBox);
+      article.insertAdjacentElement('beforeend', userBox);
+      article.insertAdjacentElement('beforeend', descriptionBox);
+      return article;
+    } else {
+      return null;
+    }
+  };
+  const getPlaces = filter => {
+    const placesFetcher = new Promise((resolve, reject) => {
+      $.ajax({
+        url: `${API_URL}/places_search`,
+        type: 'POST',
+        data: JSON.stringify(filter || {}),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: data => {
+          const placeOwnerPromises = data.map(place => new Promise((resolve, reject) => {
+            $.get(`${API_URL}/users/${place.user_id}`, (data, status) => {
+              const fullPlace = place;
+              fullPlace.user = data;
+              resolve(fullPlace);
+            });
+          }));
+          Promise
+            .all(placeOwnerPromises)
+            .then(places => resolve(places))
+            .catch(err => reject(err));
+        }
+      });
+    });
+    return placesFetcher;
+  };
+  const updatePlacesDisplay = filter => {
+    getPlaces(filter)
+      .then(places => {
+        $('section.places').empty();
+        $('section.places').append('<h1>Places</h1>');
+        for (let i = 0; i < places.length; i++) {
+          $('section.places').append(buildPlaceElement(places[i]));
+        }
+      });
+  };
+
+  $(selectors.amenityListItem).on('mousedown', ev => {
+    ev.target.getElementsByTagName('input')?.item(0)?.click();
+  });
+
+  $(selectors.amenityCheckBox).change(ev => {
+    const amenityId = ev.target.getAttribute('data-id');
+    const amenityName = ev.target.getAttribute('data-name');
+
+    if (ev.target.checked) {
+      if (!selectedAmenities.find(obj => obj.id === amenityId)) {
+        selectedAmenities.push({
+          id: amenityId,
+          name: amenityName
+        });
+      }
+    } else {
+      selectedAmenities = selectedAmenities.filter(
+        obj => (obj.id !== amenityId) && (obj.name !== amenityName)
+      );
+    }
+    const htmlContent = selectedAmenities.map(obj => obj.name).join(', ');
+    $(selectors.amenitiesHeader).html(
+      selectedAmenities.length > 0 ? htmlContent : '&nbsp;'
+    );
+  });
+
+  $.get(`${API_URL}/status`, (data, status) => {
+    if ((status === 'success') && (data.status === 'OK')) {
+      if (!$('div#api_status').hasClass('available')) {
+        $('div#api_status').addClass('available');
+      }
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+
+  updatePlacesDisplay({});
+});
